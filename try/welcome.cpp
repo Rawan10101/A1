66c2a3c0f5f92a4c4c864ad75509f40a9ecaa75b
fatimanageh <151041354+fatimanageh@users.noreply.github.com>
2024-03-06 16:58:40 +0200

    Created welcome.cpp

--- try/welcome.cpp
+++ try/welcome.cpp
@@ -1,32 +1,9 @@
 #include "welcome.h"
 #include "ui_welcome.h"
 #include "login.h"
-
-Welcome::Welcome(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::Welcome)
-{
-    //QString agestr=QString::number(ages);
-    ui->setupUi(this);
-    on_pushButton_clicked(); // Automatically trigger the login button
-
-
-    //imageLabel = new QLabel(this);
-    //imageLabel->setObjectName(QStringLiteral("imageLabel"));
-
-   // imageLabel->setGeometry(QRect(10, 10, 200, 200));
-   QPixmap pix("file:///C:/Users/HP/Downloads/360_F_176984023_8I82qQPmKn8TqNAZXIYMCSiwccoUiPBg.jpg");
-    int w=ui->image->width();
-   int h=ui->image->height();
-    ui->image->setPixmap(pix.scaled(w,h, Qt::KeepAspectRatio));
-
-    //imageLabel->setScaledContents(true);
-}
-
-Welcome::~Welcome()
-{
-    delete ui;
-}
+#include<QPixmap>
+#include<QVBoxLayout>
+#include<QLabel>
 void Welcome::setAge(const QString &age)
 {
     m_age = age;
@@ -36,7 +13,32 @@ void Welcome::setName(const QString &name)
 {
     m_name = name;
 
+
 }
+Welcome::Welcome(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Welcome)
+{
+
+
+    ui->setupUi(this);
+    ui->Hello->setText("Hello admin 20");
+
+    QPixmap pix ("C:/Users/fatim/Downloads/photophoto.jpg");
+    ui->image->setPixmap(pix.scaled(500,150,Qt::KeepAspectRatio));
+
+    on_pushButton_clicked();
+
+}
+
+Welcome::~Welcome()
+{
+    delete ui;
+}
+
+
+
+
 
 void Welcome::setPassword(const QString &password)
 {
@@ -69,16 +71,17 @@ void Welcome::handleRegistration()
 {
 
 }
+void Welcome::on_Hello_linkActivated(const QString &link)
+{
+    ui->Hello->setText("Hello " + m_name + " "+ m_age);
+    ui->Hello->show();
 
+}
 void Welcome::on_pushButton_clicked()
 {
     close();
 }
 
 
-void Welcome::on_Hello_linkActivated(const QString &link)
-{
-    ui->Hello->setText("Hello " + m_name + " "+ m_age);
 
-}
 
